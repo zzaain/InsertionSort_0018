5496294c85bdc77e00cb78af24c81ee672801b3a
zzaain <sheriellaghzain@gmail.com>
2026-03-12 14:55:08 +0700

    menambahkan proses pengurutan menggunakan insertion sort

--- insertion.cpp
+++ insertion.cpp
@@ -36,3 +36,20 @@ void insertionsort()
 {
     int temp;
     int j;
+for (int i = 0; i < n; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nstep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " ";
+        }
+    }
+}
